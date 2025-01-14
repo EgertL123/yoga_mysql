2972f9572bc8ec025178a40363ebf781978e24bf
Egert Linnas <egert.linnas@voco.ee>
2025-01-14 13:07:57 +0200

    Display all articles

---
+++ controllers/articles.js
@@ -0,0 +1,14 @@
+const db = require('../utils/db')
+
+const getAllArtcles = (req, res) => {
+    let sql = 'SELECT * FROM article'
+    db.query(sql, (error, result) => {
+        res.render('index', {
+            articles: result
+        })
+    })
+}
+
+module.exports = {
+    getAllArtcles
+}
