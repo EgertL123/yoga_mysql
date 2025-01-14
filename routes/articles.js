3c220591db23c7fc1451395acdd22295da3044b4
Egert Linnas <egert.linnas@voco.ee>
2025-01-14 13:31:50 +0200

    As a user i can see single yoga article. See #3

--- routes/articles.js
+++ routes/articles.js
@@ -4,5 +4,6 @@ const articleController = require('../controllers/articles')
 const router = express.Router()
 
 router.get('/', articleController.getAllArtcles)
+router.get('/article/:slug', articleController.getArticleBySlug)
 
 module.exports = router
